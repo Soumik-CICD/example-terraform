fa380a1dafb4aee54ca7b5f2be018bd46821f287
Soumik-CICD <60919299+Soumik-CICD@users.noreply.github.com>
2023-08-21 22:18:56 +0530

    Update format.tf
    EC2 Creation

--- format.tf
+++ format.tf
@@ -1,1 +1,11 @@
 # Terraform Content 
+provider "aws" {
+  region	= "us-east-1"
+  access_key	= "AKIA4BCQCB5WDE5MVP6P"
+  secret_key	= "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"
+}
+
+resource "aws_instance" "ec2" {
+  ami		= "ami-08a52ddb321b32a8c"
+  instance_type	= "t2.micro"
+}
